6a4d94638ee262c68aaa717f5151ee9e310505c8
tzulingyang <143302366+tzulingyang@users.noreply.github.com>
2023-09-22 16:53:19 -0400

    create a folder for js and add comment to js file

--- solution-hw3/js/custom_products.js
+++ solution-hw3/js/custom_products.js
@@ -1,6 +1,7 @@
 let currentProduct = document.querySelector('.detail-title').id
 let products = [['item-1', 2.49], ['item-2', 3.49], ['item-3', 2.99], ['item-4', 3.49], ['item-5', 3.99], ['item-6', 3.99]]
 
+// to identify which product it is and its price
 let totalPrice = 0;
 for (let k in products) {
     if (currentProduct === products[k][0]){
@@ -13,7 +14,10 @@ let total_glazing = 0;
 let total_pack = 1;
 let total = 0;
 
+// create an array for each glazing option and its price adaption
 let glazing_price = [['keep-original', 0], ['sugar-milk', 0], ['vanilla-milk', 0.5], ['double-chocolate', 1.5]];
+
+// function happens when something change for the dropdown list. Default: Keep original ($ 0.00)
 function glazingChange(element) {
     const glazing_change = element.value;
     for (let i of Object.keys(glazing_price)){
@@ -28,8 +32,10 @@ function glazingChange(element) {
 }
 
 
-
+// create an array for pack sizes and its price adaption
 let packsize_price = [['1', 1], ['3', 3], ['6', 5], ['12', 10]];
+
+// function happens when something change for the dropdown list. Default: 1 pack (*1)
 function packChange(element) {
     const pack_size = element.value;
     for (let j of Object.keys(packsize_price)){
