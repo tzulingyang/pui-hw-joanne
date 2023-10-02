093ef8d0cd5498253ae6cfbab9ba665914cf2d74
tzulingyang <143302366+tzulingyang@users.noreply.github.com>
2023-10-01 22:43:03 -0400

    Update rollsData.js

--- solution-hw4/js/rollsData.js
+++ solution-hw4/js/rollsData.js
@@ -55,41 +55,42 @@ let totalPrice = rolls[rollType]['basePrice'];
 console.log('base price:' + totalPrice);
 
 let total_glazing = 0;
-let glazing_choice = '';
+let glazing_choice = 'keep-original';
 let total_pack = 1;
-let pack = 0;
+let pack = '1';
 let total = 0;
 
-// create an array for each glazing option and its price adaption
-let glazing_price = [['keep-original', 0], ['sugar-milk', 0], ['vanilla-milk', 0.5], ['double-chocolate', 1.5]];
+// create an object for each glazing option and its price adaption
+const glazing_price = {
+    'keep-origianl': 0,
+    'sugar-milk': 0,
+    'vanilla-milk': 0.5,
+    'double-chocolate': 1.5
+};
 
 // function happens when something change for the dropdown list. Default: Keep original ($ 0.00)
 function glazingChange(element) {
     const glazing_change = element.value;
-    for (let i of Object.keys(glazing_price)){
-        if (glazing_change === glazing_price[i][0]){
-            total_glazing = glazing_price[i][1];
-            // console.log('glazing = ' + total_glazing);
-        }
-    }
+    total_glazing = glazing_price[glazing_change];
     total = ((totalPrice + total_glazing) * total_pack).toFixed(2);
     document.getElementById('cart-price').innerHTML = '$ ' + total;
-    glazing = glazing_change;
+    glazing_choice = glazing_change;
 }
 
 
-// create an array for pack sizes and its price adaption
-let packsize_price = [['1', 1], ['3', 3], ['6', 5], ['12', 10]];
+// create an object for pack sizes and its price adaption
+const packsize_price = {
+    '1': 1,
+    '3': 3,
+    '6': 5,
+    '12': 10
+};
 
 // function happens when something change for the dropdown list. Default: 1 pack (*1)
 function packChange(element) {
     const pack_size = element.value;
-    for (let j of Object.keys(packsize_price)){
-        if (pack_size === packsize_price[j][0]){
-            total_pack = packsize_price[j][1];
-            // console.log('pack size = ' + total_pack);
-        }
-    }
+    total_pack = packsize_price[pack_size]
+
     total = ((totalPrice + total_glazing) * total_pack).toFixed(2);
     document.getElementById('cart-price').innerHTML = '$ ' + total;
     pack = pack_size;
@@ -109,9 +110,8 @@ class Roll {
 }
 
 
-
 function AddtoCart(){
-    const New_cartitems = new Roll(rollType, glazing, pack, rolls[rollType]['basePrice'])
+    const New_cartitems = new Roll(rollType, glazing_choice, pack, rolls[rollType]['basePrice'])
 
     cart.push(New_cartitems);
     console.log(cart);
