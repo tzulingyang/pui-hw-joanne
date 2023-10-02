8cea840db1666a8c26a064911eceea3f5451bb3d
tzulingyang <143302366+tzulingyang@users.noreply.github.com>
2023-10-01 23:14:51 -0400

    Update rollsData.js

--- solution-hw4/js/rollsData.js
+++ solution-hw4/js/rollsData.js
@@ -69,7 +69,6 @@ const glazing_price = {
 // function happens when something change for the dropdown list. Default: Keep original ($ 0.00)
 function glazingChange(element) {
     const glazing_change = element.value;
-    console.log(glazing_change);
     total_glazing = glazing_price[glazing_change];
     total = ((totalPrice + total_glazing) * total_pack).toFixed(2);
     document.getElementById('cart-price').innerHTML = '$ ' + total;
